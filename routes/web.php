0319f751cecd1cb706f3677bf626b42d0c2dbb99
CEPHAL0 <xaradxarma@gmail.com>
2024-05-30 08:46:00 +0545

    Add Authentication Tweaks

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,30 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('app');
+    if (!Auth::user()) {
+        return redirect('/login');
+    }
+    return view("app");
 });
 
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name("login");
+// Route::view('/login', 'auth.login')->name("login");
 
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
+// Route::view('/register', 'auth.register')->name('register');
 
 
 
 Route::prefix('admin')->middleware(["auth", 'role:admin', 'preventBackHistory'])->group(
     function () {
-        Route::view("/home", 'admin.home')->name("admin.home");
+        Route::view("/home", 'admin.app')->name("admin.home");
     }
 );
 
 Route::middleware(["auth", 'role:user'])->group(
     function () {
-        Route::get("/home", function () {
-            return view('user.app');
-        })->name("user.home");
+        Route::view("/home", 'user.app')->name("user.home");
     }
 );
